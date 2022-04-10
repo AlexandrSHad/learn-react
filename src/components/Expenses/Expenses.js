729298c1c4b9d584b475e4442e97df05fcf7b2f0
AlexandrSHad <shadurskij@gmail.com>
2022-04-10 14:39:02 +0200

    display list dynamically, applying filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState((new Date()).getFullYear());
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
   return (
     <Card className="expenses">
@@ -18,11 +19,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onFilterChange={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 }
