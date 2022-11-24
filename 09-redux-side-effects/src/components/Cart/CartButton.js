c2ac23f21993488bf298e899d3173f54adfa1f29
AlexandrSHad <shadurskij@gmail.com>
2022-11-24 22:03:22 +0100

    implemented toggle cart

--- 09-redux-side-effects/src/components/Cart/CartButton.js
+++ 09-redux-side-effects/src/components/Cart/CartButton.js
@@ -1,10 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import classes from './CartButton.module.css';
+import { cartActions } from '../../store/cart';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const itemCount = useSelector(state => state.cart.items.length);
+
+  const clickHandler = () => {
+    dispatch(cartActions.toggleCart());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={clickHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{itemCount}</span>
     </button>
   );
 };
