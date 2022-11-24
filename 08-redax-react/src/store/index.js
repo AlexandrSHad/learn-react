3458958d0f1572916c098fe4f92fdcd21c097dbe
AlexandrSHad <shadurskij@gmail.com>
2022-11-24 12:46:35 +0100

    use @redux/toolkit with multiple slices

--- 08-redax-react/src/store/index.js
+++ 08-redax-react/src/store/index.js
@@ -1,33 +1,54 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+const authInitialState = {
+  isAuthenticated: false
+};
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: authInitialState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true
+    },
+    logout(state) {
+      state.isAuthenticated = false
+    }
+  }
+});
+
+const counterInitialState = {
   showCounter: true,
   counter: 0
 };
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: counterInitialState,
   reducers: {
-    increment: (state) => {
+    increment(state) {
       state.counter++;
     },
-    decrement: (state) => {
+    decrement(state) {
       state.counter--;
     },
-    increase: (state, action) => {
+    increase(state, action) {
       state.counter = state.counter + action.payload;
     },
-    toggleCounter: (state) => {
+    toggleCounter(state) {
       state.showCounter = !state.showCounter;
     }
   }
 });
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: {
+    auth: authSlice.reducer,
+    counter: counterSlice.reducer
+  }
 });
 
+export const authActions = authSlice.actions;
 export const counterActions = counterSlice.actions;
 
 export default store;
