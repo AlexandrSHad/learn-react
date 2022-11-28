6551b0d2892eee5e2570f3514f75d9c7e4944fc9
AlexandrSHad <shadurskij@gmail.com>
2022-11-28 17:45:21 +0100

    Use Nested route and Redirect

--- 10-react-router-dom/src/pages/Welcome.js
+++ 10-react-router-dom/src/pages/Welcome.js
@@ -1,5 +1,12 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome page</h1>
+  return <section>
+    <h1>The Welcome page</h1>
+    <Route path='/welcome/new-user'>
+      Welcome new user!
+    </Route>
+  </section>;
 };
 
 export default Welcome;
